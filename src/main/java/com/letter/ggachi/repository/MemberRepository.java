679f238c81f4935ba31e5eeae752d96263b3df2b
UHeeJoon <yhjyhj3855@gmail.com>
2024-01-31 20:18:05 +0900

    test2

--- src/main/java/com/letter/ggachi/repository/MemberRepository.java
+++ src/main/java/com/letter/ggachi/repository/MemberRepository.java
@@ -3,9 +3,11 @@ package com.letter.ggachi.repository;
 import com.letter.ggachi.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-  @Query("SELECT m FROM GGACHI_MEMBER m WHERE m.NICKNAME = :nickname")
+  @Query("SELECT m FROM Member m WHERE m.nickname = :nickname")
   Member findByNickname(String nickname);
 }
 
