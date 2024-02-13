26cd3046539915ac3c1cd8ae27f684106f0d9160
UHeeJoon <yhjyhj3855@gmail.com>
2024-02-13 20:57:52 +0900

    add: repository추가

---
+++ src/main/java/com/letter/ggachi/repository/member/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.letter.ggachi.repository.member;
+
+import com.letter.ggachi.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
