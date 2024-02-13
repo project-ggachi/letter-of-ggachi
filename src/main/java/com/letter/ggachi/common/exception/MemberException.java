19124287537f2d7248431fc01d5cffd7ddfd4a87
UHeeJoon <yhjyhj3855@gmail.com>
2024-02-13 20:38:20 +0900

    add: 멤버 exception

--- src/main/java/com/letter/ggachi/common/exception/MemberException.java
+++ src/main/java/com/letter/ggachi/common/exception/MemberException.java
@@ -1,4 +1,16 @@
 package com.letter.ggachi.common.exception;
 
+import com.letter.ggachi.common.exception.type.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
 public class MemberException {
+  private final HttpStatus httpStatus;
+  private final String message;
+
+  public MemberException(ErrorCode errorCode) {
+    this.httpStatus = errorCode.getHttpStatus();
+    this.message = errorCode.getMessage();
+  }
 }
