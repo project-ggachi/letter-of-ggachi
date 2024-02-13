c75e8e561776dc6d1dbc2179f8e61573c85d4b3d
UHeeJoon <yhjyhj3855@gmail.com>
2024-02-13 20:19:01 +0900

    생성 날짜, 수정 날짜 Period객체 생성

--- src/main/java/com/letter/ggachi/domain/base/Period.java
+++ src/main/java/com/letter/ggachi/domain/base/Period.java
@@ -1,2 +1,22 @@
-package com.letter.ggachi.domain.base;public class Period {
+package com.letter.ggachi.domain.base;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Period {
+  @CreatedDate
+  private LocalDateTime createdDate;
+  @LastModifiedDate
+  private LocalDateTime modifiedDate;
 }
