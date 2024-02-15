f44f21e521759a12ed5408a1a69fcc2a31d14e71
UHeeJoon <yhjyhj3855@gmail.com>
2024-02-15 15:06:29 +0900

    update: passwordEncoding Bean수정

--- src/main/java/com/letter/ggachi/config/CommonConfig.java
+++ src/main/java/com/letter/ggachi/config/CommonConfig.java
@@ -2,6 +2,7 @@ package com.letter.ggachi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,6 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class CommonConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    return new BCryptPasswordEncoder();
   }
 }
